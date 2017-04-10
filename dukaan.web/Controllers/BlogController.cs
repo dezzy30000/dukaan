05cce5ec1f575e6451c077938267ecdbde3e710f
= <=>
2017-04-10 01:17:31 +0100

    Started building out pages.

---
+++ dukaan.web/Controllers/BlogController.cs
@@ -0,0 +1,13 @@
+﻿using dukaan.web.Models.Content;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dukaan.web.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index(BlogContent content)
+        {
+            return View();
+        }
+    }
+}
---
+++ dukaan.web/Controllers/CartController.cs
@@ -0,0 +1,13 @@
+﻿using dukaan.web.Models.Content;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dukaan.web.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Index(CartContent content)
+        {
+            return View();
+        }
+    }
+}
