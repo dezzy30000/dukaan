28d1f1e783e49eaa16dffb66cbbdae626f112204
= <=>
2017-04-18 21:30:56 +0100

    Improved cart functionality.

--- dukaan.web/wwwroot/js/dukaan.js
+++ dukaan.web/wwwroot/js/dukaan.js
@@ -9,7 +9,12 @@ function get_width() {
 }
 
 $(function () {
-
+    var storageKey = 'simpleCart_items';
+    var dropdowncartbutton = $('#dropdown-cart');
+    var dropdowncartquantitytext = $('#dropdown-cart-quantity-text');
+    var dropdowncartcaret = $('#dropdown-cart-caret');
+    var cartlink = $('#cart-link');
+    var cartlinkquantitytext = $('#cart-link-quantity-text');
     var owlCarousels = [{ "element": $('#home_slider'), "options": { items: 1, loop: true, autoplay: true, autoplayHoverPause: true, dots: false, nav: true, navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'], } }, { "element": $('#widget_slider_new_arrivals'), "options": { items: 1, dots: false, nav: true, navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'], responsive: { 0: { items: 1, }, 480: { items: 2, }, 768: { items: 3, }, 992: { items: 1, } } } }, { "element": $('#widget_slider_best_selling'), "options": { items: 1, dots: false, nav: true, navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'], responsive: { 0: { items: 1, }, 480: { items: 2, }, 768: { items: 3, }, 992: { items: 1, } } } }, { "element": $('#product_slider'), "options": { dots: false, nav: true, navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'], responsive: { 0: { items: 1, }, 480: { items: 2, }, 768: { items: 3, }, 992: { items: 3, }, 1200: { items: 4, } } } }, { "element": $('#related_product_slider'), "options": { dots: false, nav: true, navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'], responsive: { 0: { items: 1, }, 480: { items: 2, }, 768: { items: 3, }, 992: { items: 5, }, 1200: { items: 6, } } } }, { "element": $('#brand_slider'), "options": { dots: false, nav: true, navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'], responsive: { 0: { items: 1, }, 480: { items: 2, margin: 15 }, 768: { items: 3, margin: 15 }, 992: { items: 4, margin: 30 }, 1200: { items: 6, margin: 30 } } } }];
 
     // open navigation dropdown on hover (only when width >= 768px)
@@ -60,12 +65,6 @@ $(function () {
         });
     }
 
-    var dropdowncartbutton = $('#dropdown-cart');
-    var dropdowncartquantitytext = $('#dropdown-cart-quantity-text');
-    var dropdowncartcaret = $('#dropdown-cart-caret');
-    var cartlink = $('#cart-link');
-    var cartlinkquantitytext = $('#cart-link-quantity-text');
-
     simpleCart.bind('update', function () {
         if (this.quantity() === 0) {
             cartlinkquantitytext.text('items');
@@ -82,6 +81,10 @@ $(function () {
         }
     });
 
+    simpleCart.bind('afterSave', function () {
+        Cookies.set(storageKey, localStorage.getItem(storageKey));
+    });
+
     simpleCart({
         cartColumns: [{
                 atrr: 'cartitem',
@@ -92,5 +95,4 @@ $(function () {
             }
         ]
     });
-
 });
